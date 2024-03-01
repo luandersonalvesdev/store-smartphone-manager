29e4d4c0d8f808c28371cebcde699e32d463875f
Luanderson Alves <luaoderson@gmail.com>
2024-03-01 19:51:21 -0300

    build: cors and axios added

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const { loginRoute, signupRoute, dashboardRoute } = require('./routes');
 
 const app = express();
 
+const allowedOrigins = [process.env.FRONTEND_BASE_URL || 'http://localhost:3000'];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.get('/', (_req, res) => {
   res.send('Server are healthy!');
