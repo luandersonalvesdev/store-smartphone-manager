f6c6bd21b2d6b892ff2d9cec6238f65e4d662583
Luanderson Alves <luaoderson@gmail.com>
2024-03-02 21:50:19 -0300

    build: change config database connection again

--- backend/api/config/config.js
+++ backend/api/config/config.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const enviroment = process.env.NODE_ENV || 'development';
 
 const options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || null,
-  database: process.env.DB_DATABASE || `database_${enviroment}`,
-  host: process.env.DB_URL_NO_SSL || 'localhost',
-  port: process.env.DB_PORT || 5432,
+  username: process.env.PG_DB_USERNAME || 'root',
+  password: process.env.PG_DB_PASSWORD || null,
+  database: process.env.PG_DB_DATABASE || `database_${enviroment}`,
+  host: process.env.PG_DB_HOST || 'localhost',
+  port: process.env.PG_DB_PORT || 5432,
   dialect: 'postgres',
 };
 
