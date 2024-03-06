b4dd90a559ca5bfefa24fdbf34ec06fbb7302d26
Luanderson Alves <luaoderson@gmail.com>
2024-03-06 16:33:33 -0300

    style: css: error page

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -1,17 +1,21 @@
-import { useRouteError, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function ErrorPage() {
-  const error = useRouteError();
-
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>
-        {'It looks like you got lost but don\'t worry, get back to the route by '}
-        <Link to="/">clicking here</Link>
-      </p>
-      <p>
-        <i>{error.statusText || error.message}</i>
+    <div
+      className="h-screen flex flex-col justify-center items-center"
+      id="error-page"
+    >
+      <h1 className="text-5xl mb-10">Oops!</h1>
+      <p className="text-xl">
+        {'It looks like you got lost but don\'t worry, get back to the route by'}
+        <Link
+          className="p-1 font-bold hover:bg-green-100
+          duration-300 rounded"
+          to="/"
+        >
+          clicking here
+        </Link>
       </p>
     </div>
   );
