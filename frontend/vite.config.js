a7ed67bc47904de2c50e38fa4a2a627901c007f2
Luanderson Alves <luaoderson@gmail.com>
2024-03-06 11:11:44 -0300

    fix: to fix process.env not working in production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,30 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 3000,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.js',
-    css: true,
-    reporters: ['verbose'],
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-      provider: 'v8',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: true,
+      port: 3000,
     },
-  },
-  define: {
-    'process.env': process.env,
-  },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './setupTests.js',
+      css: true,
+      reporters: ['verbose'],
+      coverage: {
+        reporter: ['text', 'json', 'html'],
+        provider: 'v8',
+      },
+    },
+    define: {
+      'process.env': env,
+    },
+  };
 });
